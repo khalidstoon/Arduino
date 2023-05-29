e8d4abf0647a2cae4a5fc34741b838620730e351
khalidstoon <65396500+khalidstoon@users.noreply.github.com>
2023-05-29 15:28:24 +0100

    Add files via upload

---
+++ lcd2for.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  for (int i=0 i<=1 i++){
+  for(int tofaha=1;tofaha<=16;tofaha++){
+    lcd.setCursor(tofaha,0);
+    
+  }}
+
+}
